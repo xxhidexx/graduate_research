610080a5b94b2c919f0cd824301f6bb62cc50f85
xxhidexx <uenohide2002@gmail.com>
2025-02-05 13:02:31 +0900

    READMEのcaluculate_coordinates.pyの使い方を更新し、ESCキーでの終了方法を追加。ファイル名を修正し、画像読み込みのパスを変更。

--- webcamera2_estimation/caluculate_coordinates.py
+++ webcamera2_estimation/caluculate_coordinates.py
@@ -4,22 +4,19 @@ import cv2
 def click_event(event, x, y, flags, param):
   if event == cv2.EVENT_LBUTTONDOWN:
     print(f'Image coordinates: ({x}, {y})')
-    with open('1207/coordinates_right_calib.txt', 'a') as f:
+    with open('1207/coordinates_right_calib__.txt', 'a') as f:
       f.write(f'Image coordinates: ({x}, {y})\n')
-    cv2.destroyAllWindows()
+    
 
 # 画像を読み込む
-img = cv2.imread('1207/camera_1.jpg', 1)
+img = cv2.imread('1207/camera_0.jpg', 1)
 
 # ウィンドウを作成し、マウスイベントをバインドする
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', click_event)
 
-# 画像を表示する
-cv2.imshow('image', img)
-
-# ユーザーが何かキーを押すまで待つ
-cv2.waitKey(0)
-
-# すべてのウィンドウを閉じる
+while True:
+    cv2.imshow('image', img)
+    if cv2.waitKey(1) & 0xFF == 27:  # ESCキー押下で終了
+        break
 cv2.destroyAllWindows()
